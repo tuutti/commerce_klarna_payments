9c64741931d88fcb312a8fdbfb6e2fa62937d945
Lari Rauno <tuutti@iki.fi>
2018-07-20 18:58:20 +0300

    Fixed capture request tests

--- tests/src/Unit/CaptureRequestTest.php
+++ tests/src/Unit/CaptureRequestTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\commerce_klarna_payments\Unit;
 
+use Drupal\commerce_klarna_payments\Klarna\Data\OrderItemInterface;
 use Drupal\commerce_klarna_payments\Klarna\Request\Order\CaptureRequest;
 use Drupal\Tests\UnitTestCase;
 
@@ -22,9 +23,43 @@ class CaptureRequestTest extends UnitTestCase {
    * @covers ::setShippingDelay
    */
   public function testToArray() {
+    $expected = [
+      'captured_amount' => 2500,
+      'description' => 'Description test',
+      'shipping_delay' => 5,
+    ];
     $request = new CaptureRequest();
+    $request->setCapturedAmount($expected['captured_amount'])
+      ->setDescription($expected['description'])
+      ->setShippingDelay($expected['shipping_delay']);
 
-    $this->assertEquals(['auto_capture' => TRUE], $request->toArray());
+    $this->assertEquals($expected, $request->toArray());
+  }
+
+  /**
+   * @covers ::setOrderItems
+   * @covers ::addOrderItem
+   */
+  public function testSetOrderItem() {
+    $orderItem = $this->getMockBuilder(OrderItemInterface::class)
+      ->getMock();
+    $orderItem->expects($this->any())
+      ->method('toArray')
+      ->willReturn([
+        'name' => '123',
+      ]);
+
+    $request = new CaptureRequest();
+    $request->setOrderItems([$orderItem]);
+
+    $orderLine = ['name' => '123'];
+
+    // Test with one order lines.
+    $this->assertEquals(['order_lines' => [$orderLine]], $request->toArray());
+
+    $request->addOrderItem($orderItem);
+    // Test with multiple order lines.
+    $this->assertEquals(['order_lines' => [$orderLine, $orderLine]], $request->toArray());
   }
 
 }
